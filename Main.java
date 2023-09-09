7a601fd8b1e3ffd3b28324890e3ebd7d8ec60286
Satish4149 <128406252+Satish4149@users.noreply.github.com>
2023-09-09 15:10:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String d[]) {
+        System.out.println("welcome" + d[0]);
+        int a, b;
+        a = Integer.parseInt(d[1]);
+        b = Integer.parseInt(d[2]);
+        System.out.println("Adddition of two num" + (a + b));
+    }
+}
